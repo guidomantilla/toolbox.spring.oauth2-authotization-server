708eff0760f99b9b1a599690957887d451f9ba26
Guido Mantilla <guido.mantilla@yahoo.com>
2020-05-01 01:25:50 -0500

    Fixes Commit

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@ PARENT_NAME=$1
 if [ -z "$PARENT_NAME" ]; then
     APP_NAME=toolbox.spring.oauth2-authotization-server
 else
-    APP_NAME=$PARENT_NAME
+    APP_NAME="$PARENT_NAME"
 fi
 
 gradle clean build \
 && mv build/libs/$(ls build/libs) build/libs/"$APP_NAME".jar \
 && docker build . -t "$APP_NAME" \
-&& docker container rm -f "$APP_NAME" \
-&& docker container run --detach --restart always \
+&& docker container rm --force "$APP_NAME"
+   docker container run --detach --restart always \
                         --publish 8443:8443 --link dev-mysql:dev-mysql \
                         --name "$APP_NAME" "$APP_NAME"
